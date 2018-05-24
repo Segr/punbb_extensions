3009ed5d097a26a4c54682bea7022bbda807145d
Segr <segr@nashalife.ru>
2018-05-24 17:03:36 +0300

    nl_last_tree - move language include to hook

--- nl_last_tree/hooks/mi_new_action__last_tree.php
+++ nl_last_tree/hooks/mi_new_action__last_tree.php
@@ -1,15 +1,6 @@
 <?php
 if (!defined('FORUM')) die();
 
-// lang
-global $lang_nl_last_tree;
-if (!isset($lang_nl_last_tree)) {
-	if (file_exists($ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php'))
-		include $ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php';
-	else
-		include $ext_info['path'].'/lang/English/'.$ext_info['id'].'.php';
-}
-
 $tpl_out = '';
 
 // 
