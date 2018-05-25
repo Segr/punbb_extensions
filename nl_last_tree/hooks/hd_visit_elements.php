a6ca82d92007476314886beb8e073018a569ccc6
Segr <segr@nashalife.ru>
2018-05-25 11:15:36 +0300

    nl_gravatar - small formatting changes

--- nl_last_tree/hooks/hd_visit_elements.php
+++ nl_last_tree/hooks/hd_visit_elements.php
@@ -3,9 +3,9 @@ if (!defined('FORUM')) die();
 
 $new_visit_links = array();
 foreach ($visit_links as $k=>$v) {
-  if ($k==='recent') {
-	$new_visit_links['last_tree'] = '<span id="last_tree"><a id="last_tree_link" href="'.forum_link($forum_url['last_tree']).'">'.$lang_nl_last_tree['Message tree link'].'</a></span>';
-  }
-  $new_visit_links[$k] = $v;
+	if ($k==='recent') {
+		$new_visit_links['last_tree'] = '<span id="last_tree"><a id="last_tree_link" href="'.forum_link($forum_url['last_tree']).'">'.$lang_nl_last_tree['Message tree link'].'</a></span>';
+	}
+	$new_visit_links[$k] = $v;
 }
 $visit_links = $new_visit_links;
