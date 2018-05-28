386e92fdf9ac5235b8ddce6b784faca194fd0cb9
Segr <segr@nashalife.ru>
2018-05-28 22:34:49 +0300

    nl_include_php - typo

--- nl_include_php/hooks/co_common.php
+++ nl_include_php/hooks/co_common.php
@@ -2,8 +2,8 @@
 if (!defined('FORUM')) die();
 
 // lang
-global $nl_include_php;
-if (!isset($nl_include_php)) {
+global $lang_nl_include_php;
+if (!isset($lang_nl_include_php)) {
 	if (file_exists($ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php'))
 		include $ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php';
 	else
