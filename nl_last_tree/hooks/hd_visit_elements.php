6a4b83ec14711a95c166b3bd6da274091a162f13
Segr <segr@nashalife.ru>
2025-12-03 21:50:26 +0300

    Загрузка языковых файлов Косметические изменения

--- nl_last_tree/hooks/hd_visit_elements.php
+++ nl_last_tree/hooks/hd_visit_elements.php
@@ -3,15 +3,26 @@ if (!defined('FORUM')) die();
 
 global $lang_nl_last_tree;
 
+if (empty($lang_nl_last_tree)) {
+	if (file_exists($ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php'))
+		include $ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php';
+	else
+		include $ext_info['path'].'/lang/English/'.$ext_info['id'].'.php';
+}
+
 $new_visit_links = array();
+
 $pos = 0;
-foreach ($visit_links as $k=>$v) {
-	if ($k==='recent') {
-		$new_visit_links['last_tree'] = '<span id="last_tree"'.(empty($pos) ? ' class="first-item"' : '').'><a id="last_tree_link" href="'.forum_link($forum_url['last_tree']).'">'.$lang_nl_last_tree['Message tree link'].'</a></span>';
+foreach ($visit_links as $k => $v) {
+	if ($k === 'recent') {
+		$new_visit_links['last_tree'] =	'<span id="last_tree"' . (empty($pos) ? ' class="first-item"' : '') . '><a id="last_tree_link" href="' . forum_link($forum_url['last_tree']) . '">' . $lang_nl_last_tree['Message tree link'] . '</a></span>';
 		$pos++;
 	}
-	if (!empty($pos)) $v = str_replace(' class="first-item"', '', $v);
+	if (!empty($pos)) {
+		$v = str_replace(' class="first-item"', '', $v);
+	}
 	$new_visit_links[$k] = $v;
 	$pos++;
 }
+
 $visit_links = $new_visit_links;
